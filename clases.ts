97882e31bac762e9310ca243a6100ac74587ae0b
Bittmen <38054257+Bittmen@users.noreply.github.com>
2018-11-01 12:37:14 -0600

    Update clases.ts

--- clases.ts
+++ clases.ts
@@ -1,9 +1,25 @@
-class GameConsoles{
+// Interface indica todos los metodos que nuestra clase tendrá
+interface allThings{
+    setGames(Xbox);
+    getGames();
+}
 
+function accesorios(item: string){
+    return function(target:Function){
+        target.prototype.compra = function(): void{
+            console.log("He compado "+ item);
+        }
+    }
+}
+
+class GameConsoles implements allThings{
+
+    //Propiedades
     private Xbox: Array<string>;
     private PS: Array<string>;
     private Nintendo: Array<string>;
 
+    //Metodos
     constructor(Xbox, PS, Nintendo){
         this.Xbox = Xbox;
         this.PS = PS;
@@ -22,4 +38,20 @@ class GameConsoles{
 var games = new GameConsoles(["Halo","GTA5","GOW"],[""],[""]);
 console.log(games);
 
-console.log(games.setGames("Otro"))
+console.log(games.setGames("Otro"));
+
+class GamesPortables extends GameConsoles{
+
+    public PSP: Array<string>;
+    public NDS: Array<string>;
+
+    setGames(PSP){
+        this.PSP = PSP;
+    }
+    getGames(){
+        return this.PSP;
+    }
+}
+
+var gamesPort = new GamesPortables([""],[""],[""]);
+gamesPort.setGames("GOWW");
